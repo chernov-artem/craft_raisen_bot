caf450af94f8b50fe0f59127b733aa8390445c9f
Artem <spb9719268@yandex.ru>
2024-03-01 11:42:59 +0300

    добавлена возможность прокачки крафта в 2 окна

--- functions2.py
+++ functions2.py
@@ -4,11 +4,11 @@ import time
 import images
 from move_functions import move_and_clic
 class Item():
-    ""
+    "класс для взаимодействия с разными изображениями"
     def __init__(self, src):
         self.src = src
 
-    def clic(self, xd = 0, yd = 0):
+    def clic(self, xd = 0, yd = 0, delay = 0):
         xy_tmp = images.find_coordinates(str('images/' + self.src))
         if xy_tmp != None:
             x, y = xy_tmp[0], xy_tmp[1]
@@ -16,7 +16,53 @@ class Item():
             move_and_clic(x + 24 + xd, y + 8 + yd)
         else:
             move_and_clic(1200, 500)
-        time.sleep(1.5)
+        time.sleep(1.5 + delay)
+
+class Instructions():
+    "класс инструкций"
+
+    @classmethod
+    def switch_window(cls, window = 1):
+        aion_icon.clic()
+        time.sleep(2)
+        if window == 1:
+            mouse.move(-77, -77, False)
+            time.sleep(1)
+            mouse.click('left')
+        else:
+            mouse.move(77, -77, False)
+            time.sleep(1)
+            mouse.click('left')
+
+    @classmethod
+    def craft_risen(cls):
+        "основной метод для прокачки крафта"
+        npc.clic(0, 0, 3)
+        orders_btn.clic()
+        ready_order.clic()
+        contune.clic()
+        npc.clic()
+        orders_btn.clic()
+        take_order.clic()
+        agree_btn.clic()
+        table.clic(0, 0, 4)
+        learned.clic(120, 155)
+        learned.clic(130, 100)
+        create_all_btn.clic()
+
+    @classmethod
+    def two_window_craft(cls):
+        'метод прокачки крафта в 2 окна'
+        Instructions.switch_window()
+        Instructions.craft_risen()
+        time.sleep(2)
+        Instructions.switch_window(2)
+        Instructions.craft_risen()
+        time.sleep(35)
+
+
+
+
 
 
 
@@ -35,5 +81,7 @@ create_all_btn = Item('create_all.png')
 
 time.sleep(2)
 # learned.clic(130, 100)
-aion_icon.clic()
-mouse.move(-77, -77, False)
+# for i in range(33):
+#     Instructions.two_window_craft()
+
+agree_btn.clic()
