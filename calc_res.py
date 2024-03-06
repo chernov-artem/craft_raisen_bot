a9d7907f428aedaa3feabdfec8f37105a0d10407
Artem <spb9719268@yandex.ru>
2024-03-06 20:33:25 +0300

    созданы базовые классы

--- calc_res.py
+++ calc_res.py
@@ -7,3 +7,27 @@ class Res():
         self.name = name
         self.cost = cost
 
+class Blacksmith(Res):
+    'класс кузн дела'
+    pass
+
+class Arms(Res):
+    'класс оружейки'
+    pass
+
+class Cooking(Res):
+    'класс кулинарки'
+    pass
+
+class Alhem(Res):
+    'класс алхимии'
+    pass
+
+class Sew(Res):
+    'класс портняжки'
+    pass
+
+class Jew(Res):
+    'класс ювелирки'
+    pass
+
